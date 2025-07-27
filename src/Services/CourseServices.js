8713a634c220a5cf0fa5ab89de4261a4cc4b78ea
SureshNMS <sureshbabu.pammi@neudesic.com>
2025-07-28 00:34:11 +0530

    updated keys

--- src/Services/CourseServices.js
+++ src/Services/CourseServices.js
@@ -4,9 +4,12 @@ import { doc, setDoc, getFirestore } from "firebase/firestore";
 import detailedCourses from "../Utils/DetailedCourseData.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNrq0D-mn3Dgj8GFdDhv2kAabs5kIpuJI",
-  authDomain: "coursemanagement-42a4a.firebaseapp.com",
-  projectId: "coursemanagement-42a4a",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
